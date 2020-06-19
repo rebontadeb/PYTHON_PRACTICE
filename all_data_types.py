a26e515eb44dbeca262a0e575412b02a0c8c2139
rebontadeb <rebontadeb@gmail.com>
2020-06-19 11:23:58 +0530

    Create all_data_types.py

---
+++ all_data_types.py
@@ -0,0 +1,16 @@
+str_x = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Et tortor consequat id porta. Tempus egestas sed sed risus pretium quam. Enim neque volutpat ac tincidunt vitae semper quis. Non blandit massa enim nec dui nunc mattis. Quisque egestas diam in arcu cursus euismod quis. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Consectetur a erat nam at lectus urna duis convallis. Tellus at urna condimentum mattis pellentesque id nibh tortor id"
+int_x = 20
+float_x = 20.5
+complex_x = 1j
+list_x = ["BEAFFOOSTO", "MOOVAUKE", "CAMUKLAAPS", "PANDASTRO", "PITYPUS", "BLEONYTE", "BUREEWING", "TAARSMADILLO", "SIVOOD", "VLEXAUDEA", "PANUPINE",
+          "SPARRING", "LEORSESE", "FLASULO", "CAMEXUPSU", "ELEPHUXEOCSI", "MOLAMEL", "TIKSEON", "TEESRAY", "WINEEKING", "FUPSITU", "CEEQAP", "DOLAMANDER", "BISOSAUR"]
+tuple_x = ("STUBBSMATERIAL", "ARCHEANSWEAT", "ABSOLUTEJUNO", "FUVETUSING", "ASKPAVING", "FEVERRESUBMIT", "SPLINTSQUALM", "LICKERNYLON", "WANTINGWHICH", "LOWDOTTED", "SPACKETLIMBIC", "HOOKEDREPAVE", "TAPPINGSASSY", "PICASSONIFTY", "COMMENTUSABLE",
+           "MAGNETCHECK", "VANCOUVERDEACON", "ESTATEENGINEERING", "SANDINGDEPARTMENT", "PICKLELEATHER", "SHELBYSTATE", "DADTROY", "EXISTSLIVERWURST", "ONYXREMAIN", "BUMBUGKODY", "PATRICKBOINK", "EXQUISITEFOAM", "MOTOCROSSTERRI", "NONZEROMANCERS", "FEBRUARYRAY")
+range_x = range(6)
+dict_x = {"name": "John", "age": 36}
+set_x = {"apple", "banana", "cherry"}
+frozenset_x = frozenset({"apple", "banana", "cherry"})
+bool_x = True
+bytes_x = b"Hello"
+bytearray_x = bytearray(5)
+memoryview_x = memoryview(bytes(5))
